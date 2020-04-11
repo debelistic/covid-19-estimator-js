d59881545c82dd4b6d6f18276c2df6196b8986e9
Victor Awotidebe <victorawotidebe@gmail.com>
2020-04-11 03:40:16 +0100

    ## challenge 1
    - calculate currentlyInfected
    - calculate infectionsByRequestedTime

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,24 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const { reportedCases, timeToElapse } = data;
+  const numberOfDays = timeToElapse / 3;
+
+  const impact = {
+    currentlyInfected: reportedCases * 10,
+    infectionsByRequestedTime: (2 ** numberOfDays) * (reportedCases * 10)
+  };
+
+  const severeImpact = {
+    currentlyInfected: reportedCases * 50,
+    infectionsByRequestedTime: (2 ** numberOfDays) * (reportedCases * 50)
+  };
+
+  return {
+    data,
+    estimate: {
+      impact,
+      severeImpact
+    }
+  };
+};
 
 export default covid19ImpactEstimator;
