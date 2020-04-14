2d32b30f966078dfedb47d36e02b3b1e4a7deeaf
Victor Awotidebe <victorawotidebe@gmail.com>
2020-04-14 14:05:16 +0100

    fix days logic

--- src/estimator.js
+++ src/estimator.js
@@ -6,13 +6,14 @@ const covid19ImpactEstimator = (data) => {
   let duration;
 
   if (periodType === 'days') {
-    duration = Math.trunc(timeToElapse / 3);
+    duration = timeToElapse;
   } else if (periodType === 'weeks') {
-    duration = Math.trunc((timeToElapse / 3) * 7);
+    duration = timeToElapse * 7;
   } else if (periodType === 'months') {
-    duration = Math.trunc((timeToElapse / 3) / 30);
+    duration = timeToElapse * 30;
   }
-
+  const factor = Math.trunc(duration/3);
+  const estimate = 2 ** factor;
   let dollarRate;
 
   if (periodType === 'days') {
@@ -26,24 +27,24 @@ const covid19ImpactEstimator = (data) => {
 
   const impact = {
     currentlyInfected: reportedCases * 10,
-    infectionsByRequestedTime: (2 ** duration) * (reportedCases * 10),
-    severeCasesByRequestedTime: 0.15 * (2 ** duration) * (reportedCases * 10),
+    infectionsByRequestedTime: estimate * (reportedCases * 10),
+    severeCasesByRequestedTime: 0.15 * estimate * (reportedCases * 10),
     hospitalBedsByRequestedTime: 0.35 * totalHospitalBeds
-      * (0.15 * (2 ** duration) * (reportedCases * 10)),
-    casesForICUByRequestedTime: 0.05 * (2 ** duration) * (reportedCases * 10),
-    casesForVentilatorsByRequestedTime: 0.02 * (2 ** duration) * (reportedCases * 10),
-    dollarsInFlight: (2 ** duration) * (reportedCases * 10) * avgDailyIncomeUSD * dollarRate
+      * (0.15 * estimate * (reportedCases * 10)),
+    casesForICUByRequestedTime: 0.05 * estimate * (reportedCases * 10),
+    casesForVentilatorsByRequestedTime: 0.02 * estimate * (reportedCases * 10),
+    dollarsInFlight: estimate * (reportedCases * 10) * avgDailyIncomeUSD * dollarRate
   };
 
   const severeImpact = {
     currentlyInfected: reportedCases * 50,
-    infectionsByRequestedTime: (2 ** duration) * (reportedCases * 50),
-    severeCasesByRequestedTime: 0.15 * (2 ** duration) * (reportedCases * 50),
+    infectionsByRequestedTime: estimate * (reportedCases * 50),
+    severeCasesByRequestedTime: 0.15 * estimate * (reportedCases * 50),
     hospitalBedsByRequestedTime: 0.35 * totalHospitalBeds
-      * (0.15 * (2 ** duration) * (reportedCases * 50)),
-    casesForICUByRequestedTime: 0.05 * (2 ** duration) * (reportedCases * 50),
-    casesForVentilatorsByRequestedTime: 0.02 * (2 ** duration) * (reportedCases * 50),
-    dollarsInFlight: (2 ** duration) * (reportedCases * 50) * avgDailyIncomeUSD * dollarRate
+      * (0.15 * estimate * (reportedCases * 50)),
+    casesForICUByRequestedTime: 0.05 * estimate * (reportedCases * 50),
+    casesForVentilatorsByRequestedTime: 0.02 * estimate * (reportedCases * 50),
+    dollarsInFlight: estimate * (reportedCases * 50) * avgDailyIncomeUSD * dollarRate
   };
 
 
