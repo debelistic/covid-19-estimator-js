50d87d61a270ecfe6935e498ad337f03edbcc0e4
Victor Awotidebe <victorawotidebe@gmail.com>
2020-04-14 12:35:25 +0100

    add server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import estimator from './estimator';
+
+const app = express();
+const port = 3000;
+// const server = http.createServer();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// estimator();
+
+// finally, let's start our server...
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+export default app;
